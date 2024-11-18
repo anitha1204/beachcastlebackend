b26163e305edbf902b1ac923fc82b522ee67df7a
anitha1204 <selvam12042003@gmail.com>
2024-11-18 13:45:31 +0530

    main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -774,11 +774,11 @@ const Header = () => {
             <img
               src={currentContent.img}
               alt="Banner"
-              className="object-cover rounded-2xl p-2 transition-transform transform duration-500 w-full h-[600px] lg:w-[1450px] lg:h-[600px]"
+              className="object-cover rounded-2xl p-2 transition-transform transform duration-500 h-[180px] lg:w-[1450px] lg:h-[600px]"
             />
 
             {/* Text Overlay */}
-            <div className="absolute object-cover rounded-xl inset-0 flex flex-col w-[305px] h-[585px] ml-2 mt-2 items-center justify-center text-white bg-black bg-opacity-40 p-5 lg:w-[1435px] lg:h-[582px]">
+            <div className="absolute object-cover rounded-xl inset-0 flex flex-col w-[305px] h-[585px] ml-2 mt-2 items-center justify-center text-white bg-black bg-opacity-30 p-5 lg:w-[1435px] lg:h-[582px]">
               <h2 className="lg:text-5xl text-3xl font-semibold text-center">{currentContent.heading}</h2>
               <p className="hidden lg:block lg:text-2xl mt-6 text-center lg:w-[750px]">{currentContent.description}</p>
               <button className="text-white bg-black py-3 px-9 rounded-[50px] shadow-lg transition-transform duration-300 transform hover:scale-105 mt-4"
